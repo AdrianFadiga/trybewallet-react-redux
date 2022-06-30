0e8bba725e2344b3499d9be66e4403eb31e8748c
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-06-30 00:59:57 -0300

    feat: implementado css da pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { Button, Form } from 'react-bootstrap';
 import { USER_LOGIN } from '../actions';
 import { validatePassword } from '../helpers';
+import style from './Login.module.css';
+import walletLogo from '../images/logoWallet.png';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -24,29 +27,55 @@ function Login() {
   }, [email, password]);
 
   return (
-    <section className="loginPage">
-      <div className="loginContainer">
-        <form>
-          <input
-            type="email"
-            data-testid="email-input"
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
+    <section className={style.loginPage}>
+      <div className={style.loginContainer}>
+        <div className={style.logoContainer}>
+          <img
+            className={style.logoImg}
+            src={walletLogo}
+            width="200px"
+            alt=""
           />
-          <input
-            type="password"
-            data-testid="password-input"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <button
-            type="button"
-            disabled={isDisabled}
-            onClick={handleClick}
-          >
-            Entrar
-          </button>
-        </form>
+        </div>
+        <Form className={style.loginForm}>
+          <Form.Group className="mb-2">
+            <Form.Label className="mb-2">
+              Email address
+            </Form.Label>
+            <Form.Control
+              className="mb-2"
+              type="email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-mute">
+              We&apos;ll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-2">
+            <Form.Label className="mb-2">
+              Password
+            </Form.Label>
+            <Form.Control
+              className="mb-2"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              placeholder="Password"
+            />
+          </Form.Group>
+          <div className={style.buttonContainer}>
+            <Button
+              className={style.loginButton}
+              variant="outline-success"
+              disabled={isDisabled}
+              onClick={handleClick}
+            >
+              Entrar
+            </Button>
+          </div>
+        </Form>
       </div>
     </section>
   );
